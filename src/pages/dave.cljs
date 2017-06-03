80f0398349aa4426aa6c69ceda1f2c3647856a16
Amelia Schmidt <meeli.jane@gmail.com>
2017-06-03 14:42:03 +1000

    typography updates/refinements

--- src/pages/dave.cljs
+++ src/pages/dave.cljs
@@ -27,7 +27,7 @@
 
   (layout.header-block/header
    "David Meister (Dave)"
-   "Owner and CTO at Dim Valley pty. ltd. and Nice Feature pty. ltd.")
+   "Owner and CTO at Dim Valley Pty. Ltd. & Nice Feature Pty. Ltd.")
 
   (layout.content-block/content-inner
    (h/h2 "I believe...")
@@ -38,36 +38,31 @@
 
   (layout.content-block/content-inner
    (h/h2 "Experience")
-
    (h/h3 "Commercial")
-   (layout.body-text/p (h/strong "10+ years total") " professional web experience.")
-   (layout.body-text/p (h/strong "Started out") " making cool MySpace pages for local indie bands.")
-   (layout.body-text/p (h/strong "3 years building") " " (wheel.link.hoplon/external "http://furstmedia.com.au/" "Furst Media's") " publishing platform (10% MoM growth).")
-   (layout.body-text/p (h/strong "5 years owning and operating") " my previous digital agency, " (wheel.link.hoplon/external (wheel.abn.core/abr-search-url "58147061121") "Godel") ".")
-   (layout.body-text/p (h/strong "2 years as chief solutions architect") " at " (wheel.link.hoplon/external "http://equiem.com.au/" "Equiem") " " (wheel.link.hoplon/external "http://equiem.com.au/2016" "(100% YoY growth)") ".")
-   (layout.body-text/p (h/strong "Current owner and CTO") " of Dim Valley and Nice Feature.")
-
+   (h/ul
+    (h/li (h/strong "10+ years total") " professional web experience.")
+    (h/li (h/strong "Started out") " making cool MySpace pages for local indie bands.")
+    (h/li (h/strong "3 years building") " " (wheel.link.hoplon/external "http://furstmedia.com.au/" "Furst Media's") " publishing platform (10% MoM growth).")
+    (h/li (h/strong "5 years owning and operating") " my previous digital agency, " (wheel.link.hoplon/external (wheel.abn.core/abr-search-url "58147061121") "Godel") ".")
+    (h/li (h/strong "2 years as chief solutions architect") " at " (wheel.link.hoplon/external "http://equiem.com.au/" "Equiem") " " (wheel.link.hoplon/external "http://equiem.com.au/2016" "(100% YoY growth)") ".")
+    (h/li (h/strong "Current owner and CTO") " of Dim Valley and Nice Feature."))
    (h/h3 "Open source")
-   (layout.body-text/p
-    (h/strong (wheel.link.hoplon/external "http://drupalcores.com/countries.html" "6th in Australia"))
-    " for contributions to " (wheel.link.hoplon/external "https://www.drupal.org/" "Drupal") " core.")
-
-   (layout.body-text/p
-    (h/strong "Fully automated " (wheel.link.hoplon/external "https://github.com/thedavidmeister/radiant-brook" "bitcoin trading bot"))
-    " written in " (wheel.link.hoplon/external "https://symfony.com/" "PHP/Symfony") ".")
-
-
-   (layout.body-text/p
-    (h/strong (wheel.link.hoplon/external "http://xoroshiro.di.unimi.it/" "Xoroshiro128+") " pseudorandom number generator")
-    " implemented " (wheel.link.hoplon/external "https://github.com/thedavidmeister/xoroshiro128" "in clojure") ".")
-
-   (layout.body-text/p
-    (h/strong "The " (wheel.link.hoplon/external "https://hoplon.io/" "Hoplon") " web framework")
-    ", contributing to development, support and documentation.")
-
-   (layout.body-text/p
-    (h/strong (wheel.link.hoplon/external "https://github.com/thedavidmeister/dim-valley" "This website"))
-    " built in " (wheel.link.hoplon/external "https://hoplon.io/" "Hoplon") ", leveraging a ton of " (wheel.link.hoplon/external "https://github.com/thedavidmeister/wheel" "my own intellectual property") "."))
+   (h/ul
+    (h/li
+     (h/strong (wheel.link.hoplon/external "http://drupalcores.com/countries.html" "6th in Australia"))
+     " for contributions to " (wheel.link.hoplon/external "https://www.drupal.org/" "Drupal") " core.")
+    (h/li
+     (h/strong "Fully automated " (wheel.link.hoplon/external "https://github.com/thedavidmeister/radiant-brook" "bitcoin trading bot"))
+     " written in " (wheel.link.hoplon/external "https://symfony.com/" "PHP/Symfony") ".")
+    (h/li
+     (h/strong (wheel.link.hoplon/external "http://xoroshiro.di.unimi.it/" "Xoroshiro128+") " pseudorandom number generator")
+     " implemented " (wheel.link.hoplon/external "https://github.com/thedavidmeister/xoroshiro128" "in clojure") ".")
+    (h/li
+     (h/strong "The " (wheel.link.hoplon/external "https://hoplon.io/" "Hoplon") " web framework")
+     ", contributing to development, support and documentation.")
+    (h/li
+     (h/strong (wheel.link.hoplon/external "https://github.com/thedavidmeister/dim-valley" "This website"))
+     " built in " (wheel.link.hoplon/external "https://hoplon.io/" "Hoplon") ", leveraging a ton of " (wheel.link.hoplon/external "https://github.com/thedavidmeister/wheel" "my own intellectual property") ".")))
 
   (layout.content-block/content-inner
    (h/h2 "Availability")
@@ -95,7 +90,8 @@
    (layout.body-text/p
     "The Estimate Work product is being bootstrapped by Milly and myself and is 100% self-funded. Many key features are still in planning or active development stages. Regardless, our experience with the chosen tech stack and early feedback from the target market are both positive.")
    (layout.body-text/p
-    (h/ul (h/h4 "Estimate Work's tech stack:")
+    (h/h4 "Estimate Work's tech stack:")
+    (h/ul
      (h/li (wheel.link.hoplon/external "https://clojure.org/" "Clojure") " + " (wheel.link.hoplon/external "https://clojurescript.org/" "ClojureScript") " for server and client side programming")
      (h/li (wheel.link.hoplon/external "https://blog.estimate-work.com/a-new-world-writing-css-in-clojurescript-and-life-after-sass-bdf5bc80a24f" "Garden CSS preprocessor/abstraction"))
      (h/li (wheel.link.hoplon/external "http://hoplon.io/" "Hoplon") " web framework")
@@ -103,4 +99,5 @@
      (h/li (wheel.link.hoplon/external "https://www.rethinkdb.com/" "RethinkDB") " as a back-end data persistence layer")
      (h/li (wheel.link.hoplon/external "https://auth0.com/" "Auth0") " for authentiation/SSO")
      (h/li (wheel.link.hoplon/external "https://stripe.com/au" "Stripe") " as payment gateway")
-     (h/li (wheel.link.hoplon/external "https://github.com/ptaoussanis/sente" "AJAX + WebSockets") " for client/server communication"))))])
+     (h/li (wheel.link.hoplon/external "https://github.com/ptaoussanis/sente" "AJAX + WebSockets") " for client/server communication"))
+    (layout.spacer/vertical-spacer)))])
