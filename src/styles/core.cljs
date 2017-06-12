ad8c1a5cd2b1c430ec89a0caabf4ab1519482f12
Amelia Schmidt <meeli.jane@gmail.com>
2017-06-12 21:03:20 +1000

    special somethin

--- src/styles/core.cljs
+++ src/styles/core.cljs
@@ -20,18 +20,38 @@ html {
 }
 
 body {
-    margin: 0;
-    padding: 0;
-    font-size: 1.9rem;
-    line-height: 2.9rem;
-    font-family: 'Raleway', sans-serif;
-    color: " colours.ui-gradients/base-colour ";
+ margin: 0;
+ padding: 0;
+ font-size: 1.4rem;
+ line-height: 2.9rem;
+ font-family: 'Open Sans', sans-serif;
+ color: " colours.ui-gradients/base-colour ";
+}
+
+body > table:after {
+ content: '';
+ width: 100%;
+ height: 100%;
+ position: fixed;
+ bottom: 0;
+ top: 0;
+ left: 0;
+ right: 0;
+ background-image: url('http://www.supercoloring.com/sites/default/files/silhouettes/2015/05/mountain-range-outline-silhouette.svg');
+ background-position: bottom;
+ z-index: -1;
 }
 
 h1, h2, h3, h4, h5, h6 {
   font-weight: normal;
-  font-family: 'Playfair Display', serif;
-  margin-top: 4.4rem;
+  font-family: 'Gentium Basic', serif;
+  margin-top: 2.4rem;
+}
+
+h1 {
+ font-size: 4.8rem;
+ line-height: 5.2rem;
+ font-weight: bold;
 }
 
 ul {
