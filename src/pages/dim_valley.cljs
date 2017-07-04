fe6e12797305421a07d4d88cc889632723e3cafe
Amelia Schmidt <meeli.jane@gmail.com>
2017-07-04 21:01:24 +1000

    split contact and about into two pages

--- src/pages/dim_valley.cljs
+++ src/pages/dim_valley.cljs
@@ -6,53 +6,10 @@
   layout.header-block
   layout.content-block
   layout.spacer
-  mapbox.dom
-  mapbox.api
-  [unit.conversion :as u]
   wheel.math.geometry
   wheel.abn.hoplon
   wheel.address.hoplon
   address.config))
-
-(defn dv-marker [width]
-
- (let [; cos(45) = marker-width / line-length
-       ; line-length = marker-width / cos(45)
-       line-length (j/cell= (/ width (.cos js/Math (wheel.math.geometry/degrees->radians 45))))
-       base-css (j/cell= {:width (u/n->px line-length)
-                          :height (u/n->px (/ width 6))
-                          :background-color colours.ui-gradients/base-colour
-                          :position "absolute"
-                          :top 0
-                          :left 0})]
-  (h/div
-   :css {:position "relative"}
-   ;line 1
-   (h/div
-    :css (j/cell= (merge base-css
-                         {:transform "rotate(45deg)"})))
-
-   ; line 2
-   (h/div
-    :css (j/cell= (merge base-css
-                         {:transform "rotate(-45deg)"}))))))
-
-(defn dv-map
- []
- (let [ll (mapbox.api/lng-lat 144.995415 -37.826018)
-       marker-width 20
-       options {"center" ll
-                "style" "mapbox://styles/thedavidmeister/cj2ll9ozt00282rqny3ozjigd"
-                "zoom" 13
-                "interactive" false
-                "logoPosition" "bottom-right"}
-       el (mapbox.dom/map :css {:width "100%"
-                                :height "400px"
-                                :margin-bottom "4px"}
-                          :options options
-                          :marker (dv-marker marker-width)
-                          :marker-options {:offset (map (comp - #(/ % 2)) [marker-width marker-width])})]
-  el))
 
 (defn content []
  [
@@ -66,16 +23,4 @@
    "Dim Valley"
    "Modern business services")
   (layout.content-block/content-inner
-   (layout.body-text/p "Milly Schmidt and Dave Meister have developed a business-first approach to technology. We work with organisations in an honest and bespoke manner, leveraging various approaches, patterns, technologies and tools to solve their unique problems.")
-   (layout.spacer/vertical-spacer))
-  (layout.content-block/content-inner
-   (h/h2 "Contact us")
-   (h/p (h/strong "Milly: ") "0403 858 811 | meeli.jane@gmail.com")
-   (h/p (h/strong "Dave: ") "0434 016 790 | thedavidmeister@gmail.com")
-   (wheel.address.hoplon/simple address.config/address)
-   (h/br)
-   (h/br)
-   ["A.B.N. " (wheel.abn.hoplon/abn "38 617 641 595")]
-   (h/br)
-   (layout.spacer/vertical-spacer))
-  (dv-map)])
+   (layout.body-text/p "Milly Schmidt and Dave Meister have developed a business-first approach to technology. We work with organisations in an honest and bespoke manner, leveraging various approaches, patterns, technologies and tools to solve their unique problems."))])
