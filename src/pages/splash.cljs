a58ed694347df356d6f94f0ba130ade58d1bee02
Amelia Schmidt <meeli.jane@gmail.com>
2017-08-28 16:07:50 +1000

    tagline?

--- src/pages/splash.cljs
+++ src/pages/splash.cljs
@@ -13,6 +13,6 @@
  (h/div :css {:text-align "center"
               :margin-top "30vh"}
   (type.title/h1 "Dim Valley")
-  (type.subtitle/p "A modern business consultancy")
+  (type.subtitle/p "Solving problems for digital businesses")
   (layout.spacer/vertical-spacer)
   (type.button/cta "Work with us" :about)))
