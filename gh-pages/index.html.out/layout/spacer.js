623bd75d9389c3cbac6b0b763ab6996b321bc5b8
Circle CI <thedavidmeister@gmail.com>
2017-09-03 14:05:29 +0000

    Deploy to github pages

--- gh-pages/index.html.out/layout/spacer.js
+++ gh-pages/index.html.out/layout/spacer.js
@@ -5,23 +5,23 @@ goog.require('cljs.core.constants');
 goog.require('layout.config');
 goog.require('hoplon.core');
 layout.spacer.vertical_spacer = (function layout$spacer$vertical_spacer(var_args){
-var args23463 = [];
-var len__8981__auto___23468 = arguments.length;
-var i__8982__auto___23469 = (0);
+var args23468 = [];
+var len__8981__auto___23473 = arguments.length;
+var i__8982__auto___23474 = (0);
 while(true){
-if((i__8982__auto___23469 < len__8981__auto___23468)){
-args23463.push((arguments[i__8982__auto___23469]));
+if((i__8982__auto___23474 < len__8981__auto___23473)){
+args23468.push((arguments[i__8982__auto___23474]));
 
-var G__23470 = (i__8982__auto___23469 + (1));
-i__8982__auto___23469 = G__23470;
+var G__23475 = (i__8982__auto___23474 + (1));
+i__8982__auto___23474 = G__23475;
 continue;
 } else {
 }
 break;
 }
 
-var G__23465 = args23463.length;
-switch (G__23465) {
+var G__23470 = args23468.length;
+switch (G__23470) {
 case 0:
 return layout.spacer.vertical_spacer.cljs$core$IFn$_invoke$arity$0();
 
@@ -31,7 +31,7 @@ return layout.spacer.vertical_spacer.cljs$core$IFn$_invoke$arity$1((arguments[(0
 
 break;
 default:
-throw (new Error([cljs.core.str.cljs$core$IFn$_invoke$arity$1("Invalid arity: "),cljs.core.str.cljs$core$IFn$_invoke$arity$1(args23463.length)].join('')));
+throw (new Error([cljs.core.str.cljs$core$IFn$_invoke$arity$1("Invalid arity: "),cljs.core.str.cljs$core$IFn$_invoke$arity$1(args23468.length)].join('')));
 
 }
 });
@@ -48,9 +48,9 @@ return or__7760__auto__;
 return layout.config.spacer_height;
 }
 })();
-var G__23466 = cljs.core.cst$kw$css;
-var G__23467 = new cljs.core.PersistentArrayMap(null, 1, [cljs.core.cst$kw$height,height__$1], null);
-return (hoplon.core.div.cljs$core$IFn$_invoke$arity$2 ? hoplon.core.div.cljs$core$IFn$_invoke$arity$2(G__23466,G__23467) : hoplon.core.div.call(null,G__23466,G__23467));
+var G__23471 = cljs.core.cst$kw$css;
+var G__23472 = new cljs.core.PersistentArrayMap(null, 1, [cljs.core.cst$kw$height,height__$1], null);
+return (hoplon.core.div.cljs$core$IFn$_invoke$arity$2 ? hoplon.core.div.cljs$core$IFn$_invoke$arity$2(G__23471,G__23472) : hoplon.core.div.call(null,G__23471,G__23472));
 });
 
 layout.spacer.vertical_spacer.cljs$lang$maxFixedArity = 1;
