895b1c3cf9dca163eb434431867e2a2a09b969cc
David Meister <thedavidmeister@gmail.com>
2017-09-19 14:48:41 +1000

    fix scope

--- src/contact/hoplon.cljs
+++ src/contact/hoplon.cljs
@@ -9,20 +9,20 @@
 (defn details
  []
  (let [email-subject "Dim Valley enquiry"
-       email-body "Hi,\nI saw the Dim Valley website and want to organise a time to discuss my project :)"])
- [
-  (h/p
-   (h/strong "Milly: ")
-   (wheel.phone.hoplon/phone (:phone contact.data/milly)) " | "
-   (wheel.email.hoplon/email
-    :address (:email contact.data/milly)
-    :subject email-subject
-    :body email-body))
-  (h/p
-   (h/strong "Dave: ")
-   (wheel.phone.hoplon/phone (:phone contact.data/dave)) " | "
-   (wheel.email.hoplon/email
-    :address (:email contact.data/dave)
-    :subject email-subject
-    :body email-body))
-  (h/p (h/strong "We are based at ") (wheel.address.hoplon/simple contact.data/address))])
+       email-body "Hi,\nI saw the Dim Valley website and want to organise a time to discuss my project :)"]
+  [
+   (h/p
+    (h/strong "Milly: ")
+    (wheel.phone.hoplon/phone (:phone contact.data/milly)) " | "
+    (wheel.email.hoplon/email
+     :address (:email contact.data/milly)
+     :subject email-subject
+     :body email-body))
+   (h/p
+    (h/strong "Dave: ")
+    (wheel.phone.hoplon/phone (:phone contact.data/dave)) " | "
+    (wheel.email.hoplon/email
+     :address (:email contact.data/dave)
+     :subject email-subject
+     :body email-body))
+   (h/p (h/strong "We are based at ") (wheel.address.hoplon/simple contact.data/address))]))
