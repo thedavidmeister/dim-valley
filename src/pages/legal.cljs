c980a2375ed60f532a72fce03a1a422314e3955c
David Meister <thedavidmeister@gmail.com>
2018-08-12 11:41:44 +1000

    updated COC

--- src/pages/legal.cljs
+++ src/pages/legal.cljs
@@ -66,4 +66,4 @@
     (h/br)
     "Public & Products Liability limit: " (h/strong "A$20,000,000")
     (h/br)
-    "Certificate of Currency (2017-08-08): " (wheel.link.hoplon/external "https://s3-ap-southeast-2.amazonaws.com/dim-valley-public/legal-docs/Certificate+of+currency+2017-8-8+P-IT016234517I-3.pdf" "Dim Valley Pty. Ltd. & Nice Feature Pty. Ltd.")))))
+    "Certificate of Currency (2017-08-08): " (wheel.link.hoplon/external "https://s3-ap-southeast-2.amazonaws.com/dim-valley-public/legal-docs/Certificate+of+Currency.pdf" "Dim Valley Pty. Ltd. & Nice Feature Pty. Ltd.")))))
